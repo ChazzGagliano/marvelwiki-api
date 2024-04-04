43be2b003fbf718bf842053ebd7038bbdd8b6afb
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-04-04 19:48:31 -0400

    start authentication

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import characterRoutes from "./character.js"
 import comicRoutes from "./comic.js"
 import storieRoutes from "./storie.js"
+// import userRoutes from "./user.js"
 import eventRoutes from "./event.js"
 
 const constructorMethod = (app) => {
@@ -8,6 +9,7 @@ const constructorMethod = (app) => {
     app.use("/comic", comicRoutes);
     app.use("/storie", storieRoutes);
     app.use("/event", eventRoutes)
+    // app.use("/user", userRoutes)
     app.use("*", (req, res) => {
       res.status(404).json({ error: "This route does not exist" });
     });
