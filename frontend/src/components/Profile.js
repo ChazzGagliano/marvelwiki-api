f83c8ed294c64a22f2e6c68993e987e47f084f18
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-07-01 17:20:23 -0400

    logout button styling

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -9,7 +9,6 @@ const Profile = () => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
-
   useEffect(() => {
     async function fetchData() {
       const data = await axios.get(`http://localhost:3030/user/profile`, {
@@ -21,7 +20,7 @@ const Profile = () => {
     }
 
     if (user === null) {
-        fetchData()
+      fetchData();
     }
   }, [user]);
 
@@ -40,91 +39,94 @@ const Profile = () => {
 
   const handleDeleteAccount = async () => {
     try {
-      await axios.delete('http://localhost:3030/user/delete-account', {
-        withCredentials: true
+      await axios.delete("http://localhost:3030/user/delete-account", {
+        withCredentials: true,
       });
-      window.location.href = "/profile"; 
+      window.location.href = "/profile";
     } catch (error) {
-      console.error('Error deleting account:', error);
+      console.error("Error deleting account:", error);
     }
   };
 
   const handleClick = async (event) => {
     event.preventDefault();
     const session = await axios.get(`http://localhost:3030/user/logout`, {
-        withCredentials: true
-      });
-      console.log(session)
+      withCredentials: true,
+    });
+    console.log(session);
     window.location.href = "/profile";
-};
+  };
 
   if (loading) {
     return <Loading />;
   } else {
-      if (user.data.error) {
-          return (
-              <div>
-                <Link className="linkage-2" to={`/signup`}>
-                  <h1>New? Sign up!</h1>
-                </Link>
-              </div>
-          )
-      } else {
-          return (
-              <div>
+    if (user.data.error) {
+      return (
         <div>
-          <h1>{user.data.user.username}</h1>
+          <div>
+            <Link className="linkage-2" to={`/signup`}>
+              <h1>New? Sign up!</h1>
+            </Link>
+          </div>
+          <div>
+            <Link className="linkage-2" to={`/login`}>
+              <h1>Already a Member? Login!</h1>
+            </Link>
+          </div>
         </div>
-        <h1 className="headline">Starred</h1>
-        <button
-        className="delete-account-button"
-        type="button"
-        onClick={handleDeleteAccount} 
-        >
-                    Delete Account
-                </button>
-                <div>
-                <a className="nav-link" href="#" onClick={handleClick}>
-      Logout
-    </a>
-                </div>
+      );
+    } else {
+      return (
         <div>
-          {user.data.user.characters.map((c) => {
+          <div>
+            <h1>{user.data.user.username}</h1>
+            <div>
+              <button className="logout" onClick={handleClick} type="button">
+                <img
+                  className="logout-button"
+                  src="https://cdn1.iconfinder.com/data/icons/heroicons-ui/24/logout-512.png"
+                />
+              </button>
+            </div>
+          </div>
+          <h1 className="headline">Starred</h1>
+          <button className="" type="button" onClick={handleDeleteAccount}>
+            Delete Account
+          </button>
+          <div>
+            {user.data.user.characters.map((c) => {
               return (
+                <div>
                   <div>
-                <div>
-
-                <Link to={`/characters/${c.characterId}`}>
-                  {<img src={c.characterImage} className="img" />}
-                </Link>
+                    <Link to={`/characters/${c.characterId}`}>
+                      {<img src={c.characterImage} className="img" />}
+                    </Link>
+                  </div>
+                  <div className="headline">{c.characterName}</div>
+                  <div>
+                    <button
+                      className="delete"
+                      type="button"
+                      onClick={() =>
+                        handleUnlike(
+                          c.characterId,
+                          c.characterName,
+                          c.characterImage
+                        )
+                      }
+                    >
+                      <img
+                        className="button"
+                        src="https://www.svgrepo.com/show/21045/delete-button.svg"
+                      />
+                    </button>
+                  </div>
                 </div>
-                <div className="headline">
-                    {c.characterName}
-                </div>
-                <div>
-                <button
-                  className="delete"
-                  type="button"
-                  onClick={() =>
-                    handleUnlike(
-                        c.characterId,
-                        c.characterName,
-                        c.characterImage
-                    )
-                }
-                >
-                  <img
-                    className="button"
-                    src="https://www.svgrepo.com/show/21045/delete-button.svg"
-                    />
-                </button>
-                </div>
-              </div>
-            );
-        })}
+              );
+            })}
+          </div>
         </div>
-      </div>
-    );
+      );
     }
   }
 };
