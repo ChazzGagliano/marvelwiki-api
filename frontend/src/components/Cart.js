5547a3ce4faddd995e400f28491ae087ac593a68
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-07-12 17:18:23 -0400

    add total

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -6,70 +6,78 @@ import Loading from "./Loading";
 import { Link } from "react-router-dom";
 
 const Cart = () => {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
-  
-    useEffect(() => {
-      async function fetchData() {
-        const data = await axios.get(`http://localhost:3030/user/cart`, {
-          withCredentials: true,
-        });
-        setUser(data);
-        console.log(data);
-        setLoading(false);
-      }
-  
-      if (user === null) {
-        fetchData();
-      }
-    }, [user]);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [total, setTotal] = useState(0);
 
-    if (loading) {
-        return <Loading />;
-      } else {
-        if (user.data.error) {
-          return (
-            <div>
-              <div>
-                <Link className="linkage-2" to={`/signup`}>
-                  <h1>New? Sign up!</h1>
-                </Link>
-              </div>
-              <div>
-                <Link className="linkage-2" to={`/login`}>
-                  <h1>Already a Member? Login!</h1>
-                </Link>
-              </div>
-            </div>
-          );
-        } else {
-          return (
-            <div>
-                <div>
-                    <h1>Cart</h1>
-                </div>
-                {user.data.user.cart.map((c) => {
-                    return (
-                        <div>
-                        <div>
-                            <ul>{c.comicTitle}</ul>
-                        </div>
-                        <div>
-                            <ul>${c.comicPrice}</ul>
-                        </div>
-                        <div>
-                        <img
-                        src={c.comicImage}
-                        />
-                        </div>
-                        </div>
-                    )
-                })}
-            </div>
-          );
-        }
+  useEffect(() => {
+    async function fetchData() {
+      const data = await axios.get(`http://localhost:3030/user/cart`, {
+        withCredentials: true,
+      });
+      setUser(data);
+      console.log(data);
+
+      let calculatedTotal = 0;
+      for (let i = 0; i < data.data.user.cart.length; i++) {
+        calculatedTotal += data.data.user.cart[i].comicPrice;
       }
-    };
-    
-    export default Cart;
-    
+      setTotal(calculatedTotal);
+      console.log(calculatedTotal);
+      setLoading(false);
+
+      setLoading(false);
+    }
+
+    if (user === null) {
+      fetchData();
+    }
+  }, [user]);
+
+  if (loading) {
+    return <Loading />;
+  } else {
+    if (user.data.error) {
+      return (
+        <div>
+          <div>
+            <Link className="linkage-2" to={`/signup`}>
+              <h1>New? Sign up!</h1>
+            </Link>
+          </div>
+          <div>
+            <Link className="linkage-2" to={`/login`}>
+              <h1>Already a Member? Login!</h1>
+            </Link>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <div>
+            <h1>Cart</h1>
+          </div>
+          {user.data.user.cart.map((c) => {
+            return (
+              <div>
+                <div>
+                  <ul>{c.comicTitle}</ul>
+                </div>
+                <div>
+                  <ul>${c.comicPrice}</ul>
+                </div>
+                <div>
+                  <img src={c.comicImage} />
+                </div>
+              </div>
+            );
+          })}
+          <div>total: ${total}</div>
+        </div>
+      );
+    }
+  }
+};
+
+export default Cart;
