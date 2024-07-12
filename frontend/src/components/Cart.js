84e842a45cdc43017f2619815222713040e48d4a
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-07-12 16:57:20 -0400

    set up Cart page

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -23,7 +23,51 @@ const Cart = () => {
         fetchData();
       }
     }, [user]);
-  
-}
 
-export default Cart
+    if (loading) {
+        return <Loading />;
+      } else {
+        if (user.data.error) {
+          return (
+            <div>
+              <div>
+                <Link className="linkage-2" to={`/signup`}>
+                  <h1>New? Sign up!</h1>
+                </Link>
+              </div>
+              <div>
+                <Link className="linkage-2" to={`/login`}>
+                  <h1>Already a Member? Login!</h1>
+                </Link>
+              </div>
+            </div>
+          );
+        } else {
+          return (
+            <div>
+                {user.data.user.cart.map((c) => {
+                    return (
+                        <div>
+                        <div>
+                            <il>{c.comicTitle}</il>
+                        </div>
+                        <div>
+                            <il>${c.comicPrice}</il>
+                        </div>
+                        <div>
+                        http://i.annihil.us/u/prod/marvel/i/mg/9/40/5109ab5908cce
+                        <img
+                        src={c.comicImage}
+                        />
+                        </div>
+                        </div>
+                    )
+                })}
+            </div>
+          );
+        }
+      }
+    };
+    
+    export default Cart;
+    
