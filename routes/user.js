a040a541237e894f8451d87dd43a317454833d0c
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-07-21 17:32:33 -0400

    clean code

--- routes/user.js
+++ routes/user.js
@@ -53,19 +53,17 @@ router.post("/login", async (req, res) => {
 });
 
 router.get("/logout", async (req, res) => {
-  console.log(req.session);
-  if (req.session.user) {
-    req.session.destroy((err) => {
-      if (err) {
-        return res
-          .status(500)
-          .json({ error: "Failed to log out. Please try again." });
-      }
-      return res.status(200).json({ message: "Logged out successfully" });
-    });
-  } else {
-    return res.status(401).json({ error: "You are not logged in!" });
-  }
+    console.log(req.session)
+    if (req.session.user) {
+        req.session.destroy((err) => {
+            if (err) {
+                return res.status(500).json({ error: "Failed to log out. Please try again." });
+            }
+            return res.status(200).json({ message: "Logged out successfully" });
+        });
+    } else {
+        return res.status(401).json({ error: "You are not logged in!" });
+    }
 });
 
 router.get("/profile", async (req, res) => {
@@ -83,27 +81,26 @@ router.get("/profile", async (req, res) => {
 });
 
 router.delete("/delete-account", async (req, res) => {
-  if (req.session.user) {
-    try {
-      const userCollection = await users();
-      const result = await userCollection.deleteOne({
-        _id: new ObjectId(req.session.user._id),
-      });
-
-      if (result.deletedCount === 1) {
-        req.session.destroy();
-        return res.status(200).json({ message: "User deleted successfully" });
+    if (req.session.user) {
+      try {
+        const userCollection = await users();
+        const result = await userCollection.deleteOne({
+          _id: new ObjectId(req.session.user._id),
+        });
+  
+        if (result.deletedCount === 1) {
+            req.session.destroy(); 
+            return res.status(200).json({ message: "User deleted successfully" });
+          }
+        } catch (error) {
+          console.error("Error deleting user:", error);
+          return res.status(500).json({ error: "An error occurred while deleting the user" });
+        }
+      } else {
+        return res.status(401).json({ error: "You are not logged in!" });
       }
-    } catch (error) {
-      console.error("Error deleting user:", error);
-      return res
-        .status(500)
-        .json({ error: "An error occurred while deleting the user" });
-    }
-  } else {
-    return res.status(401).json({ error: "You are not logged in!" });
-  }
-});
+  });
+  
 
 router.post("/character/like", async (req, res) => {
   console.log(req.session);
@@ -131,134 +128,135 @@ router.post("/character/like", async (req, res) => {
 });
 
 router.post("/character/unlike", async (req, res) => {
-  console.log(req.session);
-  const userCollection = await users();
-  const userId = req.session.user._id;
-  const { characterId } = req.body;
-  const { characterName } = req.body;
-  const { characterImage } = req.body;
-
-  await userCollection.updateOne(
-    { _id: new ObjectId(userId) },
-    {
-      $pull: {
-        characters: {
-          characterId: characterId,
-          characterName: characterName,
-          characterImage: characterImage,
+    console.log(req.session);
+    const userCollection = await users();
+    const userId = req.session.user._id;
+    const { characterId } = req.body;
+    const { characterName } = req.body;
+    const { characterImage } = req.body;
+  
+    await userCollection.updateOne(
+      { _id: new ObjectId(userId) },
+      {
+        $pull: {
+          characters: {
+            characterId: characterId,
+            characterName: characterName,
+            characterImage: characterImage,
+          },
         },
-      },
-    }
-  );
-  res.json({
-    success: `User ${userId} removed character ${characterId}`,
+      }
+    );
+    res.json({
+      success: `User ${userId} removed character ${characterId}`,
+    });
   });
-});
 
-router.post("/cart/add", async (req, res) => {
-  console.log(req.session);
-  const userCollection = await users();
-  const userId = req.session.user._id;
-  const { comicId } = req.body;
-  const { comicTitle } = req.body;
-  const { comicPrice } = req.body;
-  const { comicImage } = req.body;
+  router.post("/cart/add", async (req, res) => {
+    console.log(req.session);
+    const userCollection = await users()
+    const userId = req.session.user._id;
+    const { comicId } = req.body;
+    const { comicTitle } = req.body;
+    const { comicPrice } = req.body;
+    const { comicImage } = req.body
 
-  await userCollection.updateOne(
-    { _id: new ObjectId(userId) },
-    {
-      $push: {
-        cart: {
-          comicId: comicId,
-          comicTitle: comicTitle,
-          comicPrice: comicPrice,
-          comicImage: comicImage,
-        },
-      },
-    }
-  );
-  res.json({
-    success: `User ${userId} added ${comicId} to cart`,
-  });
-});
+    await userCollection.updateOne(
+        { _id: new ObjectId(userId) },
+        {
+            $push: {
+                cart: {
+                    comicId: comicId,
+                    comicTitle: comicTitle,
+                    comicPrice: comicPrice,
+                    comicImage: comicImage,
+                }
+            }
+        }
+    )
+    res.json({
+        success: `User ${userId} added ${comicId} to cart`,
+      });
+})
 
 router.post("/cart/remove", async (req, res) => {
-  console.log(req.session);
-  const userCollection = await users();
-  const userId = req.session.user._id;
-  const { comicId } = req.body;
-  const { comicTitle } = req.body;
-  const { comicPrice } = req.body;
-  const { comicImage } = req.body;
+    console.log(req.session);
+    const userCollection = await users()
+    const userId = req.session.user._id;
+    const { comicId } = req.body;
+    const { comicTitle } = req.body;
+    const { comicPrice } = req.body;
+    const { comicImage } = req.body
 
-  await userCollection.updateOne(
-    { _id: new ObjectId(userId) },
-    {
-      $pull: {
-        cart: {
-          comicId: comicId,
-          comicTitle: comicTitle,
-          comicPrice: comicPrice,
-          comicImage: comicImage,
-        },
-      },
-    }
-  );
-  res.json({
-    success: `User ${userId} added ${comicId} to cart`,
-  });
-});
+    await userCollection.updateOne(
+        { _id: new ObjectId(userId) },
+        {
+            $pull: {
+                cart: {
+                    comicId: comicId,
+                    comicTitle: comicTitle,
+                    comicPrice: comicPrice,
+                    comicImage: comicImage,
+                }
+            }
+        }
+    )
+    res.json({
+        success: `User ${userId} added ${comicId} to cart`,
+      });
+})
 
 router.post("/cart/order", async (req, res) => {
-  console.log(req.session);
-  const userCollection = await users();
-  const userId = req.session.user._id;
-  const { cart } = req.body;
+    console.log(req.session);
+    const userCollection = await users();
+    const userId = req.session.user._id;
+    const { cart } = req.body;
 
-  await userCollection.updateOne(
-    { _id: new ObjectId(userId) },
-    { $set: { cart: [] } }
-  );
-  res.json({
-    success: `Order placed, ${cart} removed`,
+    await userCollection.updateOne(
+      { _id: new ObjectId(userId) },
+      { $set: { cart: [] } }
+    );
+    res.json({
+      success: `Order placed, ${cart} removed`,
+    });
   });
-});
 
 router.get("/cart", async (req, res) => {
-  console.log(req.session);
-  if (req.session.user) {
-    const userCollection = await users();
-    const user = await userCollection.findOne({
-      _id: new ObjectId(req.session.user._id),
-    });
-
-    return res.json({ user: user });
-  } else {
-    return res.json({ error: "You are not logged in!" });
-  }
-});
-
-router.post("/add/order", async (req, res) => {
-  console.log(req.session);
-  const userCollection = await users();
-  const userId = req.session.user._id;
-  const { cart } = req.body;
-  const { totalPlusShipping } = req.body;
-
-  await userCollection.updateOne(
-    { _id: new ObjectId(userId) },
-    {
-      $push: {
-        order: {
-          cart: cart,
-          totalPlusShipping: totalPlusShipping,
-        },
-      },
+    console.log(req.session);
+    if (req.session.user) {
+      const userCollection = await users();
+      const user = await userCollection.findOne({
+        _id: new ObjectId(req.session.user._id),
+      });
+  
+      return res.json({ user: user });
+    } else {
+      return res.json({ error: "You are not logged in!" });
     }
-  );
-  res.json({
-    success: `User ${userId} order is on the way!`,
   });
-});
+
+  router.post("/add/order", async (req, res) => {
+    console.log(req.session)
+    const userCollection = await users()
+    const userId = req.session.user._id
+    const { cart } = req.body
+    const { totalPlusShipping } = req.body
+
+    await userCollection.updateOne(
+        { _id: new ObjectId(userId) },
+        {
+            $push: {
+                order: {
+                    cart: cart,
+                    totalPlusShipping: totalPlusShipping,
+                }
+            }
+        }
+    )
+    res.json({
+        success: `User ${userId} order is on the way!`,
+      });
+  })
 
 export default router;
+
